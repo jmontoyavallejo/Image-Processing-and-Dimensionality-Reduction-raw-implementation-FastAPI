564d307f9b399c289bc3ce425601ec349d2ac28a
Juan Pablo Montoya <101369659+jmontoyavallejo@users.noreply.github.com>
2023-04-04 11:17:01 -0500

    update unsupervised and add responses to main

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI
 from app.modules.matrix import Matrix
-from app.modules.Unsupervised import PCA_Training_scikit_unsupervised_module,TSNE_Training_unsupervised_module,plot_PCA_TSNE_unsupervised_module
+from app.modules.Unsupervised import PCA_Training_unsupervised_module,TSNE_Training_unsupervised_module,plot_PCA_TSNE_unsupervised_module,SVD_unsupervised_module
 from app.modules.Picture import Pictures
-from fastapi.responses import FileResponse,HTMLResponse
 from app.modules.scikit_learn_methods import load_mnist_dataset,Train_model_scikit_learn,PCA_Training_scikit_learn,TSNE_Training_scikit_learn,plot_PCA_TSNE_scikit
+
+from fastapi import FastAPI
+from fastapi.responses import FileResponse,HTMLResponse
+
 
 app = FastAPI()
 
@@ -54,9 +56,11 @@ async def compare_my_image_to_average_image():
     pass
 
 
-@app.post("/apply_SDV_MyImage")
+@app.get("/apply_SDV_MyImage")
 async def apply_SDV_MyImage():
-    pass
+    pictures = Pictures()
+    SVD_unsupervised_module(pictures.cara0)
+    return FileResponse('app/resources/SDV_image.png')
 
 
 @app.get("/Train_mnist_model_scikit_learn")
@@ -72,11 +76,6 @@ async def Train_mnist_model_local_unsupervised():
     return FileResponse('app/resources/pca_vs_tsne_scikit.png')
 
 
-@app.get("/plot_PCA_TSNE_my_unsupervised_module")
-async def plot_PCA_TSNE_my_unsupervised_module():
-    plot_PCA_TSNE_unsupervised_module()
-    return FileResponse('app/resources/pca_vs_tsne_my_unsupervised_module.png')
-    
 @app.get("/accuracy_PCA_TSNE_scikit_learn")
 async def accuracy_PCA_TSNE_scikit_learn():
     x_train,y_train,x_test,y_test=load_mnist_dataset()
@@ -85,24 +84,35 @@ async def accuracy_PCA_TSNE_scikit_learn():
     TSNE_score=TSNE_Training_scikit_learn(x_train,y_train,x_test,y_test)
     return {'normal_accuracy':normal_score,'PCA_accuracy':PCA_score,'TSNE_accuracy':TSNE_score}
 
+
+@app.get("/plot_PCA_TSNE_my_unsupervised_module")
+async def plot_PCA_TSNE_my_unsupervised_module():
+    plot_PCA_TSNE_unsupervised_module()
+    return FileResponse('app/resources/pca_vs_tsne_my_unsupervised_module.png')
+    
 @app.get("/accuracy_PCA_TSNE_my_unsupervised_module")
 async def accuracy_PCA_TSNE_scikit_learn():
     x_train,y_train,x_test,y_test=load_mnist_dataset()
     normal_score=Train_model_scikit_learn(x_train,y_train,x_test,y_test)
-    PCA_score=PCA_Training_scikit_unsupervised_module(x_train,y_train,x_test,y_test)
+    PCA_score=PCA_Training_unsupervised_module(x_train,y_train,x_test,y_test)
     TSNE_score=TSNE_Training_unsupervised_module(x_train,y_train,x_test,y_test)
     return {'normal_score':normal_score,'PCA_score':PCA_score,'TSNE_score':TSNE_score,}
 
 @app.get("/technical_methods_improve_PCA")
 async def technical_methods_improve_PCA():
-    return {'method1':'this method'}
+    return {'method1':'this method includes making more solvers to the program like random_SDV'}
 
 
 @app.get("/mathematical_principles_behind_UMAP")
 async def mathematical_principles_behind_UMAP():
-    return {'method1':'this method'}
+    return {'nerve theorem':'The nerve theorem is a mathematical principle that states that the topological structure of a space can be captured by a collection of overlapping sets, known as nerve covers. In the context of UMAP, the nerve theorem is used to construct a simplicial complex, which is a mathematical object that captures the topological structure of the data.',
+            'Graph theory':'UMAP uses a graph-based approach to construct a low-dimensional embedding of high-dimensional data. Specifically, UMAP constructs a weighted k-nearest neighbor graph, where each data point is connected to its k nearest neighbors. The weights on these edges represent the degree of similarity between the connected points.',
+            'Riemannian geometry':'UMAP constructs a low-dimensional embedding of the high-dimensional data by minimizing a cost function that is based on a Riemannian metric. The Riemannian metric is a mathematical concept from differential geometry that measures the curvature of a space. In the context of UMAP, the Riemannian metric is used to measure the distance between points in the low-dimensional space.',
+            'Topological Data Analysis':'Topological Data Analysis (TDA) is a branch of mathematics that uses algebraic topology to study the topological structure of data. In the context of UMAP, TDA is used to construct a simplicial complex, which is a topological object that captures the topological structure of the data. Specifically, UMAP uses the Mapper algorithm, which is a TDA algorithm that constructs a simplicial complex based on a cover of overlapping sets.'}
 
 
 @app.get("/mathematical_principles_behind_LDA")
 async def mathematical_principles_behind_LDA():
-    return {'method1':'this method'}
+    return {'Dirichlet distribution':'The Dirichlet distribution is a probability distribution over a simplex, which is a multi-dimensional space whose points all lie on the surface of a hyperplane. In the context of LDA, the Dirichlet distribution is used to model the distribution of topics over a set of documents. Specifically, each document is represented as a mixture of topics, and the Dirichlet distribution is used to model the probability distribution of these mixtures.',
+            'Bayesian inference':'LDA is a Bayesian model, which means that it uses Bayes theorem to perform probabilistic inference. Bayes theorem is a mathematical formula that allows us to update our beliefs about a hypothesis based on new evidence. In the context of LDA, we use Bayes theorem to infer the underlying topic structure of a set of documents. Specifically, we start with a prior belief about the topic structure, and we update this belief based on the observed words in the documents.',
+            'Matrix factorization':'LDA can be viewed as a form of matrix factorization, where we factorize a matrix of word frequencies into two matrices: one representing the distribution of topics over the words, and the other representing the distribution of documents over the topics. This factorization is achieved using Bayesian inference and optimization techniques.'}
--- app/modules/Unsupervised.py
+++ app/modules/Unsupervised.py
@@ -1,10 +1,10 @@
-import pandas as pd
+import cv2
 import numpy as np
 from keras.datasets import mnist
 from sklearn.linear_model import LogisticRegression
 import matplotlib.pyplot as plt
-
-
+from numpy.linalg import eigh,norm
+import logging
 
 class BaseEstimator:
     y_required = True
@@ -64,7 +64,7 @@ class BaseEstimator:
     def _predict(self, X=None):
         raise NotImplementedError()
 
-class PCA(BaseEstimator):
+class PCA_unsupervised_module(BaseEstimator):
     y_required = False
 
     def __init__(self, n_components, solver="svd"):
@@ -89,19 +89,24 @@ class PCA(BaseEstimator):
         self.mean = np.mean(X, axis=0)
         self._decompose(X)
 
+    def fit_transform(self, X, y=None):
+        self.fit(X) 
+        return self.transform(X)
+
     def _decompose(self, X):
         # Mean centering
         X = X.copy()
         X -= self.mean
 
         if self.solver == "svd":
-            _, s, Vh = SVD(X, full_matrices=True)
+            _, s, Vh = SVD_unsupervised_module(X, full_matrices=True)
         elif self.solver == "eigen":
             s, Vh = np.linalg.eig(np.cov(X.T))
             Vh = Vh.T
 
         s_squared = s ** 2
         variance_ratio = s_squared / s_squared.sum()
+        logging.info("Explained variance ratio: %s" % (variance_ratio[0: self.n_components]))
         self.components = Vh[0: self.n_components]
 
     def transform(self, X):
@@ -110,91 +115,220 @@ class PCA(BaseEstimator):
         return np.dot(X, self.components.T)
 
     def _predict(self, X=None):
-        return self.transform(X)
-    
-    
-class SVD:
-    def __init__(self, C=1.0, kernel='rbf', gamma='scale'):
-        self.C = C
-        self.kernel = kernel
-        self.gamma = gamma
-        self.weights = None
-        self.bias = None
+        return self.transform(X)   
 
-    def fit(self, X, y):
-        n_samples, n_features = X.shape
-        alpha = np.zeros(n_samples)
-        gram_matrix = self._compute_gram_matrix(X)
-        for _ in range(1000):
-            for i in range(n_samples):
-                error_i = np.dot(self.weights, gram_matrix[i]) + self.bias - y[i]
-                alpha_i_old = alpha[i]
-                alpha_i = alpha_i_old + error_i / gram_matrix[i,i]
-                alpha_i = max(0, min(alpha_i, self.C))
-                alpha[i] = alpha_i
-                self.weights += (alpha_i - alpha_i_old) * y[i] * X[i]
-                self.bias += y[i] - np.dot(self.weights, X[i])
-    
-    def _compute_gram_matrix(self, X):
-        return np.dot(X, X.T)
 
-    def predict(self, X):
-        return np.sign(np.dot(X, self.weights) + self.bias)
-    
-
-class TSNE:
-    def __init__(self, n_components=2, perplexity=30.0, learning_rate=200.0):
-        self.n_components = n_components
+class TSNE_unsupervised_module():
+    def __init__(self, n_components=2, perplexity=30.0, max_iter=200, learning_rate=500,seed=1):
+        """A t-Distributed Stochastic Neighbor Embedding implementation.
+        Parameters
+        ----------
+        max_iter : int, default 200
+        perplexity : float, default 30.0
+        n_components : int, default 2
+        """
+        self.max_iter = max_iter
         self.perplexity = perplexity
-        self.learning_rate = learning_rate
+        self.n_components = n_components
+        self.momentum = 0.9
+        self.min_gain = 0.01
+        self.lr = learning_rate
+        self.tol = 1e-5
+        self.seed=seed
 
-    def fit_transform(self, X):
-        n_samples = X.shape[0]
-        distances = self._compute_pairwise_distances(X)
-        P = self._compute_conditional_probabilities(distances)
-        Y = np.random.normal(size=(n_samples, self.n_components))
-        for _ in range(1000):
-            grad = self._compute_gradient(P, Y)
-            Y -= self.learning_rate * grad
-        return Y
+    def fit(self,X):
+      pass
+    def transform(self,X):
+      pass
+    def fit_transform(self,X):
+      P = self.p_joint(X, self.perplexity)
+      Y = np.random.RandomState(self.seed).normal(0., 0.0001, [X.shape[0], 2])
 
-    def _compute_pairwise_distances(self, X):
-        sum_X = np.sum(np.square(X), axis=1)
-        distances = np.add(np.add(-2 * np.dot(X, X.T), sum_X).T, sum_X)
-        return distances
-    def _compute_conditional_probabilities(self, distances):
-        P = np.zeros_like(distances)
+      # Initialise past values (used for momentum)
+      if self.momentum:
+          Y_m2 = Y.copy()
+          Y_m1 = Y.copy()
+
+      # Start gradient descent loop
+      for i in range(self.max_iter):
+
+          # Get Q and distances (distances only used for t-SNE)
+          Q, distances = self.q_tsne(Y)
+          # Estimate gradients with respect to Y
+          grads = self.tsne_grad(P, Q, Y, distances)
+
+          # Update Y
+          Y = Y - self.lr * grads
+          if self.momentum:  # Add momentum
+              Y += self.momentum * (Y_m1 - Y_m2)
+              # Update previous Y's for momentum
+              Y_m2 = Y_m1.copy()
+              Y_m1 = Y.copy()
+      return Y
+
+    def neg_squared_euc_dists(self,X):
+        """Compute matrix containing negative squared euclidean
+        distance for all pairs of points in input matrix X
+
+        # Arguments:
+            X: matrix of size NxD
+        # Returns:
+            NxN matrix D, with entry D_ij = negative squared
+            euclidean distance between rows X_i and X_j
+        """
+        # Math? See https://stackoverflow.com/questions/37009647
+        sum_X = np.sum(np.square(X), 1)
+        D = np.add(np.add(-2 * np.dot(X, X.T), sum_X).T, sum_X)
+        return -D
+
+
+    def softmax(self,X, diag_zero=True, zero_index=None):
+        """Compute softmax values for each row of matrix X."""
+
+        # Subtract max for numerical stability
+        e_x = np.exp(X - np.max(X, axis=1).reshape([-1, 1]))
+
+        # We usually want diagonal probailities to be 0.
+        if zero_index is None:
+            if diag_zero:
+                np.fill_diagonal(e_x, 0.)
+        else:
+            e_x[:, zero_index] = 0.
+
+        # Add a tiny constant for stability of log we take later
+        e_x = e_x + 1e-8  # numerical stability
+
+        return e_x / e_x.sum(axis=1).reshape([-1, 1])
+
+
+    def calc_prob_matrix(self,distances, sigmas=None, zero_index=None):
+        """Convert a distances matrix to a matrix of probabilities."""
+        if sigmas is not None:
+            two_sig_sq = 2. * np.square(sigmas.reshape((-1, 1)))
+            return self.softmax(distances / two_sig_sq, zero_index=zero_index)
+        else:
+            return self.softmax(distances, zero_index=zero_index)
+
+
+    def binary_search(eval_fn, target, tol=1e-10, max_iter=10000,
+                      lower=1e-20, upper=1000.):
+        """Perform a binary search over input values to eval_fn.
+
+        # Arguments
+            eval_fn: Function that we are optimising over.
+            target: Target value we want the function to output.
+            tol: Float, once our guess is this close to target, stop.
+            max_iter: Integer, maximum num. iterations to search for.
+            lower: Float, lower bound of search range.
+            upper: Float, upper bound of search range.
+        # Returns:
+            Float, best input value to function found during search.
+        """
+        for i in range(max_iter):
+            guess = (lower + upper) / 2.
+            val = eval_fn(guess)
+            if val > target:
+                upper = guess
+            else:
+                lower = guess
+            if np.abs(val - target) <= tol:
+                break
+        return guess
+
+
+    def calc_perplexity(self,prob_matrix):
+        """Calculate the perplexity of each row
+        of a matrix of probabilities."""
+        entropy = -np.sum(prob_matrix * np.log2(prob_matrix), 1)
+        perplexity = 2 ** entropy
+        return perplexity
+
+
+    def perplexity(self,distances, sigmas, zero_index):
+        """Wrapper function for quick calculation of
+        perplexity over a distance matrix."""
+        return self.calc_perplexity(
+            self.calc_prob_matrix(distances, sigmas, zero_index))
+
+
+    def find_optimal_sigmas(self,distances, target_perplexity):
+        """For each row of distances matrix, find sigma that results
+        in target perplexity for that role."""
+        sigmas = []
+        # For each row of the matrix (each point in our dataset)
         for i in range(distances.shape[0]):
-            sorted_indices = np.argsort(distances[i])
-            distances_i = distances[i, sorted_indices[1:self.perplexity+1]]
-            numerator = np.exp(-distances_i ** 2 / (2 * self._get_variance(distances_i)))
-            P[i, sorted_indices[1:self.perplexity+1]] = numerator / np.sum(numerator)
-        return (P + P.T) / (2 * distances.shape[0])
+            # Make fn that returns perplexity of this row given sigma
+            eval_fn = lambda sigma: \
+                self.perplexity(distances[i:i+1, :], np.array(sigma), i)
+            # Binary search over sigmas to achieve target perplexity
+            correct_sigma = self.binary_search(eval_fn, target_perplexity)
+            # Append the resulting sigma to our output array
+            sigmas.append(correct_sigma)
+        return np.array(sigmas)
 
-    def _compute_gradient(self, P, Y):
-        Q = self._compute_similarity_matrix(Y)
-        PQ_diff = P - Q
-        PQ_diff_diag = np.diag(PQ_diff)
-        t_grad = np.dot(PQ_diff, Y) - np.dot(Y, np.diag(PQ_diff_diag))
-        return 4 * t_grad * (1 + np.square(np.linalg.norm(Y, axis=1, keepdims=True))) ** -1
 
-    def _compute_similarity_matrix(self, Y):
-        distances = self._compute_pairwise_distances(Y)
-        inv_distances = (1 + distances) ** -1
-        np.fill_diagonal(inv_distances, 0)
-        return inv_distances / np.sum(inv_distances)
+    def p_conditional_to_joint(P):
+        """Given conditional probabilities matrix P, return
+        approximation of joint distribution probabilities."""
+        return (P + P.T) / (2. * P.shape[0])
 
-    def _get_variance(self, distances):
-        return np.square(distances).sum() / distances.size
 
-    def transform(self, X):
-        return self.fit_transform(X)
+    def q_tsne(self,Y):
+        """t-SNE: Given low-dimensional representations Y, compute
+        matrix of joint probabilities with entries q_ij."""
+        distances = self.neg_squared_euc_dists(Y)
+        inv_distances = np.power(1. - distances, -1)
+        np.fill_diagonal(inv_distances, 0.)
+        return inv_distances / np.sum(inv_distances), inv_distances
+
+
+    def tsne_grad(self,P, Q, Y, distances):
+        """t-SNE: Estimate the gradient of the cost with respect to Y."""
+        pq_diff = P - Q  # NxN matrix
+        pq_expanded = np.expand_dims(pq_diff, 2)  # NxNx1
+        y_diffs = np.expand_dims(Y, 1) - np.expand_dims(Y, 0)  # NxNx2
+        # Expand our distances matrix so can multiply by y_diffs
+        distances_expanded = np.expand_dims(distances, 2)  # NxNx1
+        # Weight this (NxNx2) by distances matrix (NxNx1)
+        y_diffs_wt = y_diffs * distances_expanded  # NxNx2
+        grad = 4. * (pq_expanded * y_diffs_wt).sum(1)  # Nx2
+        return grad
+
+
+    def p_joint(self,X, target_perplexity):
+        """Given a data matrix X, gives joint probabilities matrix.
+
+        # Arguments
+            X: Input data matrix.
+        # Returns:
+            P: Matrix with entries p_ij = joint probabilities.
+        """
+        # Get the negative euclidian distances matrix for our data
+        distances = self.neg_squared_euc_dists(X)
+        # Find optimal sigma for each row of this distances matrix
+        sigmas = self.find_optimal_sigmas(distances, target_perplexity)
+        # Calculate the probabilities based on these optimal sigmas
+        p_conditional = self.calc_prob_matrix(distances, sigmas)
+        # Go from conditional to joint probabilities matrix
+        P = self.p_conditional_to_joint(p_conditional)
+        return P
+    
+
+def SVD_unsupervised_module(A):
+  ev,V=eigh(A.T@A)
+  u=[]
+  for i in range(A.shape[1]):
+    u.append(A@V[:,i]/norm(A@V[:,i]))
+  U=np.array(u).T
+  S=np.round(U.T@A@V,decimals=5)
+  return U,np.diagonal(S),V.T
 
 
 def Train_model_scikit_learn(x_train,y_train,x_test,y_test):
     model = LogisticRegression()
     model.fit(x_train, y_train)
     return model.score(x_test, y_test)
+
 
 def load_mnist_dataset():
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
@@ -218,10 +352,12 @@ def plot_PCA_TSNE_unsupervised_module():
     x_train,y_train,x_test,y_test=load_mnist_dataset()
     color_train=['r' if i==0 else 'b' for i in y_train ]
     color_test = ['r' if i == 0 else 'b' for i in y_test]
-    pca = PCA(n_components=2)
-    pca_components = pca.fit_transform(x_train)
-    pca_components_test= pca.fit_transform(x_test)
-    tsne = TSNE(n_components=2)
+    pca = PCA_unsupervised_module(n_components=2)
+    pca.fit(x_train)
+    pca_components = pca.transform(x_train)
+    pca.fit(x_test)
+    pca_components_test= pca.transform(x_test)
+    tsne = TSNE_unsupervised_module(n_components=2)
     tsne_components = tsne.fit_transform(x_train)
     tsne_components_test = tsne.fit_transform(x_test)
     _, ((ax1, ax2), (ax3, ax4))  = plt.subplots(2, 2, figsize=(12, 12))
@@ -235,9 +371,11 @@ def plot_PCA_TSNE_unsupervised_module():
     ax4.set_title('t-SNE Test')
     plt.savefig('app/resources/pca_vs_tsne_scikit.png')
 
-def PCA_Training_scikit_unsupervised_module(x_train,y_train,x_test,y_test):
-    pca = PCA(n_components=2)
-    x_train_pca = pca.fit_transform(x_train)
+def PCA_Training_unsupervised_module(x_train,y_train,x_test,y_test):
+    pca = PCA_unsupervised_module(n_components=2)
+    pca.fit(x_train)
+    x_train_pca = pca.transform(x_train)
+    pca.fit(x_test)
     x_test_pca = pca.transform(x_test)
     model = LogisticRegression()
     model.fit(x_train_pca, y_train)
@@ -245,7 +383,7 @@ def PCA_Training_scikit_unsupervised_module(x_train,y_train,x_test,y_test):
 
 
 def TSNE_Training_unsupervised_module(x_train,y_train,x_test,y_test):
-    tsne = TSNE(n_components=2)
+    tsne = TSNE_unsupervised_module(n_components=2)
     x_train_tsne = tsne.fit_transform(x_train)
     x_test_tsne = tsne.fit_transform(x_test)
     model = LogisticRegression()
